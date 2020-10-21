ee49febd49fb4edbbe8e30cabe5a4039f1cc76e7
Taneli <Taneli.Nyyssola96@hotmail.com>
2020-10-21 14:19:08 +0300

    4.2 asti valmis

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const morgan = require('morgan')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
---
+++ osa4/blogilista/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        res.json(blogs)
+      })
+  })
+  
+  blogsRouter.post('/', (req, res) => {
+    const body = req.body
+  
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    })
+    blog
+      .save()
+      .then(result => {
+        res.status(201).json(result)
+      })
+  })
+
+  module.exports = blogsRouter
+  
