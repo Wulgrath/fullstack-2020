f3a1b998a880b1c59971894b99c0c89ce226bb1d
Taneli <Taneli.Nyyssola96@hotmail.com>
2020-10-28 17:03:50 +0200

    4.19 asti valmis

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -6,11 +7,23 @@ blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     res.json(blogs.map(blog => blog.toJSON()))
   })
+
+  const getTokenFrom = req => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
   
   blogsRouter.post('/', async (req, res) => {
     const body = req.body
-    const user = await User.findById(body.userId)
-    console.log(body._id)
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
     
     const blog = new Blog({
       title: body.title,
